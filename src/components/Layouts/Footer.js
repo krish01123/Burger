f4ee6d16c18a1b58cb0bc14aa2be556a867e808d
Krish <hsirkmhj02@gmail.com>
2026-05-06 17:41:43 +0545

    fix unused imports in Footer

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Carousel } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Footer() {
